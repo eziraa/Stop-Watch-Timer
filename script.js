aecbf65dd9a8262a5ed4827fc71b200a0434317b
eziraa <ezratgab@gmail.com>
2024-01-29 19:43:05 +0300

    strtTimer function and start btn event listener added

--- script.js
+++ script.js
@@ -3,10 +3,16 @@
 const minuteElm = document.querySelector(".minute");
 const secondElm = document.querySelector(".second");
 const microSecElem = document.querySelector(".micro-second");
-const btnStart = document.querySelector(".start");
-const btnStop = document.querySelector(".stop");
-const btnPause = document.querySelector(".pause");
-const btnReset = document.querySelector(".reset");
+const btnStart = document.querySelector(".btn-start");
+const btnStop = document.querySelector(".btn-stop");
+const btnPause = document.querySelector(".btn-pause");
+const btnReset = document.querySelector(".btn-reset");
 const lapsElm = document.querySelector(".labs");
 const lapsItemTitle = document.querySelector(".item-title");
 const lapsItemTime = document.querySelector(".lap-time");
+let micro_second = 0;
+const startTimer = function () {
+  setInterval(updateUi, 10);
+};
+
+btnStart.addEventListener("click", startTimer);
