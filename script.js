0db54a3f0eb6be3a575f5fc32ed2f0008ad54917
eziraa <ezratgab@gmail.com>
2024-01-29 20:04:47 +0300

    adding lap object to laps array

--- script.js
+++ script.js
@@ -7,16 +7,18 @@ const btnStart = document.querySelector(".btn-start");
 const btnStop = document.querySelector(".btn-stop");
 const btnPause = document.querySelector(".btn-pause");
 const btnReset = document.querySelector(".btn-reset");
-const lapsElm = document.querySelector(".labs");
+const lapsElm = document.querySelector(".laps");
 const lapsItemTitle = document.querySelector(".item-title");
 const lapsItemTime = document.querySelector(".lap-time");
 let micro_second = 0;
 let interval = null;
+const laps = [];
 const startTimer = function () {
   interval = setInterval(updateUi, 10);
   btnStart.disabled = true;
 };
 
+// To update the timer ui when it trigger click on start btn
 const updateUi = function () {
   micro_second++;
   let minute = Math.trunc(micro_second / 100 / 60) % 60;
@@ -28,11 +30,28 @@ const updateUi = function () {
   microSecElem.textContent = micro_sec < 10 ? `0${micro_sec}` : micro_sec;
 };
 
+// Stop timer method
 const stopTimer = function () {
-    clearInterval(interval);
-    minuteElm.textContent = "00";
-    secondElm.textContent = "00";
-    microSecElem.textContent = "00";
+  // remove interval
+  clearInterval(interval);
+  // adding lap object to laps array
+  addLap();
+
+  // clrear time elemnts textfield
+  minuteElm.textContent = "00";
+  secondElm.textContent = "00";
+  microSecElem.textContent = "00";
+};
+
+// adding lap object to laps array
+
+const addLap = function () {
+  const lap = {
+    minute: minuteElm.textContent,
+    second: secondElm.textContent,
+    micro_second: microSecElem.textContent,
+  };
+  laps.push(lap);
 };
 
 // Adding event listener
