3722417e45e65400120e502368959f3b5ca840a0
eziraa <ezratgab@gmail.com>
2024-01-29 19:15:08 +0300

    necessary UI element selected

---
+++ script.js
@@ -0,0 +1,12 @@
+// Getting UI element
+
+const minuteElm = document.querySelector(".minute");
+const secondElm = document.querySelector(".second");
+const microSecElem = document.querySelector(".micro-second");
+const btnStart = document.querySelector(".start");
+const btnStop = document.querySelector(".stop");
+const btnPause = document.querySelector(".pause");
+const btnReset = document.querySelector(".reset");
+const lapsElm = document.querySelector(".labs");
+const lapsItemTitle = document.querySelector(".item-title");
+const lapsItemTime = document.querySelector(".lap-time");
